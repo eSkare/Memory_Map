0351465d538ebe9a711699a0ce4af7010de32020
578045 <eirikskare1@gmail.com>
2025-07-25 20:54:43 +0200

    ree

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://szcotkwupwrbawgprkbk.supabase.co'; // Replace with your Project URL
-const SUPABASE_ANON_KEY = 'sb_publishable_ZQcsxoa1HmByAK0nBLT-iA_Bzs0xgwd'; // Replace with your anon key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Y290a3d1cHdyYmF3Z3Bya2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNTEyNDcsImV4cCI6MjA2ODkyNzI0N30.e-cQbi9lt803sGD-SUItopcE6WgmYcxLFgPsGFp32zI'; // Replace with your anon key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
